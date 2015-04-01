cd283209c9b19e4caa89fd48b6daf828abfab398
Grace Mekarski and Tina Ramsey <gracelauren909@gmail.com>
2015-04-01 12:19:33 -0700

    add admirers and messages views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admirers
-  root to: "users#index"
+  root to: "admirers#index"
+  resources :admirers, :except => [:index, :new, :create, :edit, :update, :destroy] do
+    resources :messages, :except => [:edit, :update, :destroy]
+  end
 end
