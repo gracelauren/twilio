d6fdef83de9ab85ac8cb07810378be5b1751ec8e
Grace Mekarski and Tina Ramsey <gracelauren909@gmail.com>
2015-04-01 16:15:19 -0700

    add views for app

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,46 @@
+class MessagesController < ApplicationController
+  before_filter :authenticate_admirer!, except: [:index, :show]
+
+  def index
+    @admirer = Admirer.find(params[:admirer_id])
+    @messages = @admirer.messages.all
+  end
+
+  def new
+    @admirer = Admirer.find(params[:admirer_id])
+    @message = @admirer.messages.new
+  end
+
+  def create
+    @admirer = Admirer.find(params[:admirer_id])
+    @message = @admirer.messages.new(message_params)
+    if @message.save
+      respond_to do |format|
+        format.html do
+          flash[:notice] = "Your message was sent."
+          redirect_to admirer_messages_path(@admirer)
+        end
+        format.js { render js: "alert('Your message sent.');"}
+      end
+    else
+      respond_to do |format|
+        format.html do
+          flash[:error] = "Oops, something went wrong."
+          render :new
+        end
+        format.js { render js: "alert('It did not send.');"}
+      end
+    end
+  end
+
+  def show
+    @admirer = Admirer.find(params[:admirer_id])
+    @message = Message.find(params[:id])
+  end
+
+  private
+
+    def message_params
+      params.require(:message).permit(:body, :to)
+    end
+end
